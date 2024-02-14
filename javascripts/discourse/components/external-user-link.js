c7651f5ba32cda7cf4711c9c233b6097aed556e8
chapoi <101828855+chapoi@users.noreply.github.com>
2024-02-14 13:20:07 +0200

    PR feedback

--- javascripts/discourse/components/external-user-link.js
+++ javascripts/discourse/components/external-user-link.js
@@ -1,7 +1,6 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { inject as service } from "@ember/service";
-import { ajax } from "discourse/lib/ajax";
 
 export default class ExternalUserLink extends Component {
   @service site;
@@ -9,6 +8,7 @@ export default class ExternalUserLink extends Component {
   @tracked externalLinkUserFieldId;
   @tracked showExternalLink = false;
   @tracked user = this.args.model.username;
+  @tracked userFields = this.args.model.user_fields;
 
   constructor() {
     super(...arguments);
@@ -29,34 +29,19 @@ export default class ExternalUserLink extends Component {
         return;
       }
 
-      this.getUserFields().then((data) => {
-        const userFields = data.user.user_fields;
-
-        this.externalLinkUserFieldId = userFields[externalLinkField.id];
-        if (!this.externalLinkUserFieldId) {
-          return;
-        } else {
-          this.showExternalLink = true;
-          const url =
-            this.addSlashIfNeeded(settings.external_link_prefix) +
-            this.externalLinkUserFieldId;
-          this.externalUserLinkUrl = url;
-        }
-      });
+      this.externalLinkUserFieldId = this.userFields[externalLinkField.id];
+      if (!this.externalLinkUserFieldId) {
+        return;
+      } else {
+        this.showExternalLink = true;
+        const url =
+          settings.external_link_prefix + this.externalLinkUserFieldId;
+        this.externalUserLinkUrl = url;
+      }
     } else {
-      const url =
-        this.addSlashIfNeeded(settings.external_link_prefix) + this.user;
+      const url = settings.external_link_prefix + this.user;
       this.externalUserLinkUrl = url;
       this.showExternalLink = true;
     }
-  }
-
-  getUserFields() {
-    let url = `/u/${this.user}.json`;
-    return ajax(url);
-  }
-
-  addSlashIfNeeded(url) {
-    return url.slice(-1) !== "/" ? url + "/" : url;
   }
 }
